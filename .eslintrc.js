3d3748573a5e21b1f0afb6276ee0f72567d5c99f
erik schievenin <erik.schievenin@gmail.com>
2022-05-09 22:05:45 +0200

    update project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['default']
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ src/components/Header.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container">
+    <div class="logo">
+      <a href="#">
+        <img src="../assets/img/dc-logo.png" alt="DC" />
+      </a>
+    </div>
+    <nav>
+      <ul>
+        <li
+          v-for="(item, i) in menuItems"
+          :key="i"
+          :class="{ active: currentItem == i }"
+        >
+          <a :href="item.url">{{ item.text }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderUp",
+  data() {
+    return {
+      menuItems: [
+        {
+          text: "Characters",
+          url: "#",
+        },
+        {
+          text: "Comics",
+          url: "#",
+        },
+        {
+          text: "Movies",
+          url: "#",
+        },
+        {
+          text: "TV",
+          url: "#",
+        },
+        {
+          text: "Games",
+          url: "#",
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+        },
+        {
+          text: "Videos",
+          url: "#",
+        },
+        {
+          text: "Fans",
+          url: "#",
+        },
+        {
+          text: "News",
+          url: "#",
+        },
+        {
+          text: "Shop",
+          url: "#",
+        },
+      ],
+      currentItem: 1,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  justify-content: space-between;
+  height: 115px;
+  padding: 0 15px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.logo {
+  a {
+    display: flex;
+    align-items: center;
+
+    img {
+      height: 81px;
+      width: 81px;
+    }
+  }
+}
+
+ul {
+  display: flex;
+  font-size: 20px;
+
+  li {
+    margin: 0 20px;
+    padding: 48px 0 43px 0;
+
+    &:hover,
+    &:active {
+      border-color: #0282f9;
+    }
+
+    a {
+      color: #1c1c1c;
+
+      &:hover {
+        color: #0282f9;
+      }
+    }
+  }
+}
+</style>
